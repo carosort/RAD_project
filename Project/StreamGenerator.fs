679d7d86ffba95018db6a43d8f67771d85a75870
carosort <carosort@gmail.com>
2026-05-21 15:33:03 +0200

    Added code from handout

---
+++ Project/StreamGenerator.fs
@@ -0,0 +1,37 @@
+module StreamGenerator
+
+// Indput: 
+//          n:  heltal, antallet af elementer i strømmen
+//          l:  heltal, beskriver antallet af forskellige nøgler i strømmen
+//
+// Specifikt vil der være 2l forskellige nøgler i strømmen og strømmen vil have længde n
+let createStream (n: int) (l: int) : seq<uint64 * int> = 
+    seq {
+        // We generate a random uint64 number.
+        let rnd = System.Random()
+
+        let mutable a = 0UL
+
+        let b : byte [] = Array.zeroCreate 8 
+        rnd.NextBytes(b)
+
+        for i = 0 to 7 do
+            a <- (a <<< 8) + uint64(b.[i])
+
+        // We demand that our random number has 30 zeros on the least
+        // significant bits and then a one.
+        a <- (a ||| ((1UL <<< 31) - 1UL)) ^^^ ((1UL <<< 30) - 1UL)
+        
+        let mutable x : uint64 = 0UL
+        for i = 1 to (n/3) do
+            x <- x + a
+            yield (x &&& (((1UL <<< l) - 1UL) <<< 30), 1)
+
+        for i = 1 to ((n + 1)/3) do
+            x <- x + a
+            yield (x &&& (((1UL <<< l) - 1UL) <<< 30), -1)
+
+        for i = 1 to (n + 2)/3 do
+            x <- x + a
+            yield (x &&& (((1UL <<< l) - 1UL) <<< 30), 1)
+    }
