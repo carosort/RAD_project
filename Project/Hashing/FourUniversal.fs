7566d98980f75ee58c3193317fc34eac45d182ff
carosort <carosort@gmail.com>
2026-05-21 17:48:31 +0200

    Udkast til 4-universel funktion tilføjet

--- Project/Hashing/FourUniversal.fs
+++ Project/Hashing/FourUniversal.fs
@@ -1,1 +1,18 @@
 module FourUniversal
+
+open System.Numerics
+
+// a:       array af heltal skarpt mindre end p
+let hashFourUniversal (a: bigint[]) (x: uint64): uint64 =
+    let p: bigint = (1I <<< 89) - 1I
+
+    let mutable y = a[0]
+
+    for i = 1 to 3 do
+        y <- y * (bigint x) + a[i]
+        y <- (y &&& p) + (y >>> 89)
+    
+    if y >= p then
+        uint64 (y - p)
+    else
+        uint64 y 
